d10b29501373557162ba18f59d89d9946db45ca6
Rohan <rohanakode52@gmail.com>
2023-07-01 00:25:06 +0530

    GET Jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,20 +1,27 @@
-const getAllJobs = async (req, res)=>{
-    res.send('get all jobs')
-}
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors/index");
 
-const getJob = async (req, res)=>{
-    res.send('get job')
-}
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+};
 
-const createJobs = async (req, res)=>{
-    res.json(req.user)
-}
+const getJob = async (req, res) => {
+  res.send("get job");
+};
 
-const updateJob = async (req, res)=>{
-    res.send('update job')
-}
+const createJobs = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
 
-const deleteJob = async (req, res)=>{
-    res.send('delete job')
-}
-module.exports = {getAllJobs, getJob, createJobs, updateJob, deleteJob}
+const updateJob = async (req, res) => {
+  res.send("update job");
+};
+
+const deleteJob = async (req, res) => {
+  res.send("delete job");
+};
+module.exports = { getAllJobs, getJob, createJobs, updateJob, deleteJob };
--- models/Job.js
+++ models/Job.js
@@ -1,29 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const JobSchema = new mongoose.Schema(
   {
     company: {
       type: String,
-      required: [true, "Please provide company name"],
+      required: [true, 'Please provide company name'],
       maxlength: 50,
     },
     position: {
       type: String,
-      required: [true, "Please provide company name"],
+      required: [true, 'Please provide position'],
       maxlength: 100,
     },
     status: {
       type: String,
-      enum: ["interview", "declined", "pending"],
-      default: "pending",
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
     },
     createdBy: {
       type: mongoose.Types.ObjectId,
-      ref: "User",
-      required: [true, "Please provide user"],
+      ref: 'User',
+      required: [true, 'Please provide user'],
     },
   },
   { timestamps: true }
-);
+)
 
-module.exports = mongoose.model("Job", JobSchema);
+module.exports = mongoose.model('Job', JobSchema)
